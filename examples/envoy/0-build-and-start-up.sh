2ad10d1698c0f57047b56bfb2f90dd928a787c5a
ryan nelson <ryan@ryan.net>
2019-08-13 13:16:08 -0700

    changed envoy startup so it restarts every 120 seconds.

--- examples/envoy/0-build-and-start-up.sh
+++ examples/envoy/0-build-and-start-up.sh
@@ -4,3 +4,17 @@
 
 docker-compose up -d
 
+echo "starting envoys... if you need to exec in, and re-start them, the start command is on web and echo at /root/start-envoy-command.sh"
+echo
+
+
+## loop and restart the web envoy every 120 seconds.
+docker exec -d $(docker ps | grep envoy_web | awk '{print $1}' ) sh -c "while true ; do timeout 120 sh -c \"bash /root/start-envoy-command.sh\" ; done"
+
+## loop and restart the echo envoy every 120 seconds.
+docker exec -d $(docker ps | grep envoy_echo | awk '{print $1}' ) sh -c "while true ; do timeout 120 sh -c \"bash /root/start-envoy-command.sh\" ; done"
+
+echo "-------" 
+docker exec -it $(docker ps | grep envoy_web | awk '{print $1}' ) sh -c "hostname ; ps -ef | grep envoy"
+docker exec -it $(docker ps | grep envoy_echo | awk '{print $1}' ) sh -c "hostname ; ps -ef | grep envoy"
+
